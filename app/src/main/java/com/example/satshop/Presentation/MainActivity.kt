848dd03593bf6e6dd79111caf8ed450d0dc54049
sator <lbvf885@gmail.com>
2022-07-01 13:57:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/satshop/Presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+ package com.example.satshop.Presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.satshop.R
+
+ class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
