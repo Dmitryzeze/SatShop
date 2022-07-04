38e644cd0e877702a03cda5e91399f546a18e35c
sator <lbvf885@gmail.com>
2022-07-04 17:25:56 +0300

    add presentation

---
+++ app/src/main/java/com/example/satshop/presentation/MainViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.satshop.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.satshop.data.ShopListRepositoryImpl
+import com.example.satshop.domain.*
+
+class MainViewModel : ViewModel() {
+    private val repository = ShopListRepositoryImpl
+    private val addShopItemUseCase = AddShopItemUseCase(repository)
+    private val getShopItemUseCase = GetShopItemUseCase(repository)
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+
+    val shopList = getShopListUseCase.getShopList()
+
+
+    fun deleteShopItem(shopItem: ShopItem) {
+
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+
+
+    }
+
+    fun editShopItem(shopItem: ShopItem) {
+        val newShopItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newShopItem)
+
+    }
+}
